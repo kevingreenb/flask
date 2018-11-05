19118333b606327e88f5837509c811e955a17417
Kevin Green <kevin.e.green@hotmail.com>
2018-11-04 20:58:30 -0600

    Flask stuff

--- Project2/app.py
+++ Project2/app.py
@@ -2,10 +2,12 @@ import json
 from flask import Flask, render_template, request, redirect, url_for, make_response
 app = Flask(__name__)
 
+from options import DEFAULTS
+
 #Retrieves name from cookie
 def get_saved_data():
 	try:
-		data = json.loads(request.cookies.get('character')) #retries any saved character cookies
+		data = json.loads(request.cookies.get('character')) #retrives any saved character cookies
 	except TypeError:
 		data = {} #else it returns empty dict
 	return data
@@ -15,10 +17,14 @@ def index():
 	data = get_saved_data() #gets saved cookies if any
 	return render_template('index.html', saves=data) #passes saves as a dictionary
 
+@app.route('/builder')
+def builder():
+	return render_template('builder.html',
+		saves=get_saved_data(), options=DEFAULTS)
 
 @app.route('/save', methods=['POST'])
 def save():
-	response = make_response(redirect(url_for('index')))
+	response = make_response(redirect(url_for('builder')))
 	data = get_saved_data() #gets saved cookies if any
 	data.update(dict(request.form.items())) #gets all key pair values from request and updates data
 	response.set_cookie('character', json.dumps(data)) #create cookie character
